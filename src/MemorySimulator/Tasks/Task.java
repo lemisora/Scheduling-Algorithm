fa2314041fa0bbfc438c359af9092344152d0b36
RainBWP <zeusxd952@gmail.com>
2024-08-23 02:20:08 -0600

    genero las funciones get para la clase Task

--- src/MemorySimulator/Tasks/Task.java
+++ src/MemorySimulator/Tasks/Task.java
@@ -1,12 +1,21 @@
 package MemorySimulator.Tasks;
 public class Task {
     private String nombre;
-    private short tamaño;
+    private short tamano;
     private int tiempo;
 
     public Task(String name, short tam, int time){
         this.nombre = name;
-        this.tamaño = tam;
+        this.tamano = tam;
         this.tiempo = time;
+    }
+    public String getName(){
+        return this.nombre;
+    }
+    public short getTamano(){
+        return this.tamano;
+    }
+    public int getTiempo(){
+        return this.tiempo;
     }
 }
