ed4444b0741435a2f4af2bef7d0d3078e945685a
lemisora <leonardo.michel.dom@gmail.com>
2024-08-22 22:10:31 -0600

    Clase Mem en el que se gestionará lo relacionado a la memoria

---
+++ src/MemorySimulator/Memory/Mem.java
@@ -0,0 +1,29 @@
+package MemorySimulator.Memory;
+public class Mem {
+    public final short MEM_RANGE 1024;
+    public final short MEM_PARTS 4;
+    public short[] memory;
+    public short prim_part = 230;
+    public short seg_part = 120;
+    public short ter_part = 500;
+    public short cuarta_part = 174;
+    public boolean[MEM_PARTS] available;
+
+    public Mem(){
+        memory = new short[MEM_RANGE];
+        available = new boolean[];
+        for(int i = 0; i < MEM_PARTS; i++)
+            available[i] = false;
+        System.out.println("Se ha inicializado la memoria correctamente");
+    }
+
+    public int Allocator(int part, String id_Task){
+        if(available[part] == false) {
+            available[part] = true;
+            return 0;
+        }else {
+            System.out.println("No se puede ejecutar el programa " + id_Task + " ya que la partición de memoria " + (part + 1) + " está ocupada");
+            return -1;
+        }
+    }
+}
