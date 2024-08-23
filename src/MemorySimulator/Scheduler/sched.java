000d545ca5010fd58e2500ee4116394a45e8a319
Rain <114127654+RainBWP@users.noreply.github.com>
2024-08-23 19:16:58 +0000

    Se agrega idea para el cuerpo del algoritmo de ordenamiento

--- src/MemorySimulator/Scheduler/sched.java
+++ src/MemorySimulator/Scheduler/sched.java
@@ -5,7 +5,12 @@ public class sched {
     * Para el algoritmo de despacho se usará un modelo FIFO (el primero que entra será el primero en salir)
     *
     * */
-    public sched(){
+    public sched(Mem memory, Task[] task){
+        System.out.println("Iniciando Algoritmo");
+        for(int i = 0; i < task.length; i++){
+            
+        }
 
     }
+
 }
