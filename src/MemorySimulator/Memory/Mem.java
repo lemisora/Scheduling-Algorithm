c05909625b3c9c7956ca67c5a96abc85b87738a7
RainBWP <114127654+RainBWP@users.noreply.github.com.>
2024-08-24 18:35:04 -0600

    Cambios a memoria que simula mas la memoria

--- src/MemorySimulator/Memory/Mem.java
+++ src/MemorySimulator/Memory/Mem.java
@@ -1,71 +1,68 @@
 package MemorySimulator.Memory;
 
-// Simulamos la memoria de la computadora
 public class Mem {
     // La memoria tiene un rango de 1024 bytes
     public final short MEM_RANGE = 1024;
+    public char[] memory; // tiene la informacion de la memoria
+    public boolean[] available; // esa parte de la memoria esta disponible
+    Partition [] partitions;
+
     public final short MEM_PARTS = 4;
-    public short[] memory;
-    public String[] memory_task_names;
     public short prim_part = 230;
     public short seg_part = 120;
     public short ter_part = 500;
     public short cuarta_part = 174;
-    public boolean[] available;
+
+
 
     
     public Mem(){
-        memory = new short[MEM_RANGE];
+        memory = new char[MEM_RANGE];
         available = new boolean[MEM_PARTS];
-        memory_task_names = new String[MEM_PARTS];
         for(int i = 0; i < MEM_PARTS; i++){
             available[i] = false;
-            memory_task_names[i] = "";
         }
+
+        // crear particiones
+        // no se como automatizar esto con lo que tenemos y necesitamos entregar
+        partitions = new Partition[MEM_PARTS];
+        
+        partitions[0] = new Partition((short) 0, prim_part);
+
+        partitions[1] = new Partition((short) prim_part, seg_part);
+
+        partitions[2] = new Partition((short) seg_part, ter_part);
+
+        partitions[3] = new Partition((short) ter_part, cuarta_part);
+      
+        // checar si no se solapan las particiones
+        // capaz borramos esto
+        for(int i = 0; i < MEM_PARTS; i++){
+            for(int j = 0; j < MEM_PARTS; j++){
+                if(i != j){
+                    if(partitions[i].Overlaps(partitions[j])){
+                        System.out.println("Error: Las particiones se solapan");
+                    }
+                }
+            }
+        }
+
         System.out.println("Se ha inicializado la memoria correctamente");
     }
 
-    public boolean Allocator(int part, String id_Task){
-        if(available[part] == false) {
-            available[part] = true;
-            memory_task_names[part] = id_Task;
-            // se regresa que si se uso la memoria
-            return true;
-        }else {
-            System.err.println("No se puede ejecutar el programa " + id_Task + " ya que la partición de memoria " + (part + 1) + " está ocupada");
-            System.err.println("El programa " + memory_task_names[part] + " está ocupando la partición de memoria " + (part + 1));
-            return false;
-        }
+    public boolean partition_is_free(short partition){
+        return partitions[partition].IsFree();
     }
 
-    // se pondra a checar si la parte de la memoria pertenece a cierta tarea?
-    public boolean Deallocator(int part, String id_Task){
-        if(available[part] == true) {
-            available[part] = false;
-            // se regresa que la memoria esta libre
-            return true;
-        }else {
-            System.err.println("No se puede liberar la memoria de la partición " + (part + 1) + " ya que no está ocupada");
-            return false;
-        }
+    public boolean partition_allocate_process(short partition, String process_name, short process_size){
+        return partitions[partition].AllocateProcess(process_name, process_size, this);
     }
 
-    // no creo que se ocupe, pero es un deslocalizador que checa que la tarea a borrar sea una dada
-    public boolean DeallocatorSecure(int part, String id_Task){
-        if(available[part] == true && memory_task_names[part].equals(id_Task)) {
-            available[part] = false;
-            // se regresa que la memoria esta libre
-            return true;
-        }else {
-            if (available[part] == false){
-                System.err.println("No se puede liberar la memoria de la partición " + (part + 1) + " ya que no está ocupada");
-            } else {
-                System.err.println("No se puede liberar la memoria de la partición " + (part + 1) + " ya que no pertenece a la tarea " + id_Task);
-                System.err.println("La partición de memoria " + (part + 1) + " pertenece a la tarea " + memory_task_names[part]);
-            }
-            return false;
-        }
+    public void partition_deallocate_process(short partition){
+        partitions[partition].DeallocateProcess(this);
     }
+
+   
 
     public static void main(String[] args) {
         Mem mem = new Mem();
