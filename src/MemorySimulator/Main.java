0f0fb5ecde5ef0c437450560a1b4e99b054f9626
lemisora <leonardo.michel.dom@gmail.com>
2024-08-26 00:57:37 -0600

    Adaptación para ejecución de pruebas de las clases en el paquete MemorySimulator.Memory

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,6 +1,19 @@
 package MemorySimulator;
+import MemorySimulator.Memory.*;
+import MemorySimulator.Tasks.Task;
 public class Main {
+    static Mem memoria;
     public static void main(String[] args) {
-
+        memoria = new Mem();
+        Task tar1 = new Task("tar1", (short) 250, 23);
+//        if(memoria.partition_allocate_process((short) 2, tar1.getName(), (short)tar1.getTamano()) != true)
+//            System.out.println("No se pudo alojar la tarea en la partición 1");
+//        else
+//            System.out.println("Alojada correctamente");
+//
+//        if(memoria.partition_allocate_process((short) 2, tar1.getName(), (short)tar1.getTamano()) != true)
+//            System.out.println("No se pudo alojar la tarea en la partición 1");
+//        else
+//            System.out.println("Alojada correctamente");
     }
 }
