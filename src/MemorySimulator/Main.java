d08239a251128ba3bb3d73615731765ced7af026
lemisora <leonardo.michel.dom@gmail.com>
2024-08-26 23:52:19 -0600

    Adaptación para pruebas

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,11 +1,13 @@
 package MemorySimulator;
 import MemorySimulator.Memory.*;
 import MemorySimulator.Tasks.Task;
+import MemorySimulator.Scheduler.sched;
 public class Main {
     static Mem memoria;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         memoria = new Mem();
-        Task tar1 = new Task("tar1", (short) 250, 23);
+        sched despacho = new sched();
+        despacho.join(10000);
 //        if(memoria.partition_allocate_process((short) 2, tar1.getName(), (short)tar1.getTamano()) != true)
 //            System.out.println("No se pudo alojar la tarea en la partición 1");
 //        else
