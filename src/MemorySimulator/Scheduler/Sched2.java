c9d36453a604413e100c0df4376455e83391ea72
anteba <jose.tejedabar@alumno.buap.mx>
2024-08-27 23:30:34 -0600

    Se utiliza la interfaz Runnable para la utilizacion de hilos.

---
+++ src/MemorySimulator/Scheduler/Sched2.java
@@ -0,0 +1,33 @@
+package MemorySimulator.Scheduler;
+import MemorySimulator.Queue.ListaLigada;
+import MemorySimulator.Queue.Nodo;
+import MemorySimulator.Tasks.Task;
+
+public class Sched2 implements Runnable {
+    private ListaLigada lis;
+    private String nombreLista;
+
+
+    public Sched2(ListaLigada lis, String nombreLista){
+        this.lis = lis;
+        this.nombreLista = nombreLista;
+    }
+
+    @Override
+    public void run(){
+        Nodo referencia = lis.getHead();
+        System.out.println("Nombre de la lista"+nombreLista );
+        while(referencia != null){
+            Task task =referencia.getTask();
+            System.out.println("Proceso actual: "+task.getName()+" Tamano: "+task.getTamano()+" Tiempo: "+task.getTiempo() + "\n ");
+            try{
+                Thread.sleep(task.getTiempo());
+            }catch(InterruptedException e){
+                System.out.println(nombreLista + "fue interrumpida");
+            }
+            referencia = referencia.getSig();
+        }
+        System.out.println(nombreLista + " ha terminado de imprimir");
+    }
+    
+}
