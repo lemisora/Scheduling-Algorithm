21b3c990df3472b01f7b67ae298832ece35875a9
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-08-28 13:50:17 -0600

    El main ya es un main

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,21 +1,19 @@
 package MemorySimulator;
 import MemorySimulator.Memory.*;
 import MemorySimulator.Tasks.Task;
+import MemorySimulator.Tasks.TaskGenerator;
+import MemorySimulator.Scheduler.Sched2;
 import MemorySimulator.Scheduler.sched;
 public class Main {
     static Mem memoria;
     public static void main(String[] args) throws InterruptedException {
-        memoria = new Mem();
-        sched despacho = new sched();
-        despacho.join(10000);
-//        if(memoria.partition_allocate_process((short) 2, tar1.getName(), (short)tar1.getTamano()) != true)
-//            System.out.println("No se pudo alojar la tarea en la partición 1");
-//        else
-//            System.out.println("Alojada correctamente");
-//
-//        if(memoria.partition_allocate_process((short) 2, tar1.getName(), (short)tar1.getTamano()) != true)
-//            System.out.println("No se pudo alojar la tarea en la partición 1");
-//        else
-//            System.out.println("Alojada correctamente");
+        //TaskGenerator.exportTasksToFile(new TaskGenerator().generateManyTasks(20));
+        
+        Sched2 sched2 = new Sched2(TaskGenerator.importTasksFromFile("tasks.txt"),"Lista Default");
+        long startTime = System.currentTimeMillis();
+        sched2.run();
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        System.out.println("Execution time: " + executionTime + " milliseconds");
     }
 }
