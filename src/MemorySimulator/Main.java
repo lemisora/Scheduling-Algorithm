1fe0218d2dd3f0fa1d9ec3f0cd6580313b4962d5
RainBWP <114127654+RainBWP@users.noreply.github.com.>
2024-08-31 22:36:14 -0600

    compatibilidad con el nuevo Sched2 y se obtiene los numeros de procesadores de la compuitadora

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,13 +1,18 @@
 package MemorySimulator;
-import MemorySimulator.Memory.*;
-import MemorySimulator.Tasks.TaskGenerator;
 import MemorySimulator.Scheduler.Sched2;
-import MemorySimulator.Scheduler.sched;
+import MemorySimulator.Tasks.TaskGenerator;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         //TaskGenerator.exportTasksToFile(new TaskGenerator().generateManyTasks(20));
         
-        Sched2 sched2 = new Sched2(TaskGenerator.importTasksFromFile("tasks.txt"),"Lista Default");
+        // Obtener el número de procesadores disponibles de la computadora
+        // puede ser necesario poner un numero menor si se quiere probar con menos procesadores
+        int numProcesadores = (Runtime.getRuntime().availableProcessors());
+
+        Sched2 sched2 = new Sched2(
+            TaskGenerator.importTasksFromFile("tasks.txt"),
+            "Lista Default",
+            numProcesadores);
         long startTime = System.currentTimeMillis();
         sched2.run();
         long endTime = System.currentTimeMillis();
