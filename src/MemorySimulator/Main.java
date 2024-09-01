14ce111f0efda9dee2575e23962c4fd1d42b3f51
RainBWP <114127654+RainBWP@users.noreply.github.com.>
2024-08-31 23:30:55 -0600

    Ahora scheduler funciona en main

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,20 +1,30 @@
 package MemorySimulator;
-import MemorySimulator.Scheduler.Sched2;
+import MemorySimulator.Memory.Mem;
+import MemorySimulator.Scheduler.*;
 import MemorySimulator.Tasks.TaskGenerator;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //TaskGenerator.exportTasksToFile(new TaskGenerator().generateManyTasks(20));
-        
         // Obtener el número de procesadores disponibles de la computadora
         // puede ser necesario poner un numero menor si se quiere probar con menos procesadores
         int numProcesadores = (Runtime.getRuntime().availableProcessors());
+        Mem memory = new Mem();
+
+        scheduler scheduler1 = new scheduler(
+            TaskGenerator.importTasksFromFile(
+            "tasks.txt"),
+            "Lista Default",
+            4,
+            memory);
+
 
         Sched2 sched2 = new Sched2(
-            TaskGenerator.importTasksFromFile("tasks.txt"),
+            TaskGenerator.importTasksFromFile(
+            "tasks.txt"),
             "Lista Default",
             numProcesadores);
+
         long startTime = System.currentTimeMillis();
-        sched2.run();
+        scheduler1.run();
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
         System.out.println("Execution time: " + executionTime + " milliseconds");
