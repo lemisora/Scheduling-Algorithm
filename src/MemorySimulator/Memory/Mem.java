0c0d00686197565dc74d3a3b7af3a11600541389
lemisora <leonardo.michel.dom@gmail.com>
2024-08-31 18:26:43 -0600

    Depuración de código y cambio de short a int en los atributos

--- src/MemorySimulator/Memory/Mem.java
+++ src/MemorySimulator/Memory/Mem.java
@@ -1,30 +1,24 @@
 package MemorySimulator.Memory;
 
 public class Mem {
-    public final short MEM_RANGE = 1024; // La memoria tiene un rango de 1024 bytes
-    public final short MEM_PARTS = 4;   //Cantidad de particiones que tendrá la memoria
+    private final int MEM_RANGE = 1024; // La memoria tiene un rango de 1024 bytes
+    private final int MEM_PARTS = 4;   //Cantidad de particiones que tendrá la memoria
 
     public char[] memory; // tiene la informacion de la memoria
-    //public boolean[] available; // Con un arreglo de booleanos se determinará que partición se encuentra disponible
     public Partition [] partitions; //Con un arreglo del objeto partition se mandarán a llamar los métodos para manejar memoria
 
     //Tamanios de las particiones
-    public short prim_part = 230;
-    public short seg_part = 120;
-    public short ter_part = 500;
-    public short cuarta_part = 174;
+    public int prim_part = 230;
+    public int seg_part = 120;
+    public int ter_part = 500;
+    public int cuarta_part = 174;
     
     public Mem(){
         memory = new char[MEM_RANGE];
-        //available = new boolean[MEM_PARTS];
-//        for(int i = 0; i < MEM_PARTS; i++){
-//            //available[i] = true;
-//        }
-
         // Crear particiones
         partitions = new Partition[MEM_PARTS];
         //La primera particion comienza en 0 y termina en 230
-        partitions[0] = new Partition((short) 0, prim_part);
+        partitions[0] = new Partition(0, prim_part);
         //La segunda particion comienza en 230 y termina en 350
         partitions[1] = new Partition(partitions[0].getMemory_end(), seg_part);
         //La tercera particion comienza en 350 y termina en 850
@@ -39,31 +33,19 @@ public class Mem {
         System.out.println("Se ha inicializado la memoria correctamente");
     }
 
-    public boolean partition_is_free(short partition){
+    public boolean partition_is_free(int partition){
         return partitions[partition].IsFree();
     }
 
-    public boolean partition_allocate_process(short partition, String process_name, short process_size){
+    public boolean partition_allocate_process(int partition, String process_name, short process_size){
         return partitions[partition].AllocateProcess(process_name, process_size, this);
     }
 
-    public void partition_deallocate_process(short partition){
+    public void partition_deallocate_process(int partition){
         partitions[partition].DeallocateProcess(this);
     }
 
-    public boolean fits_in_partition(short partition, short process_size){
+    public boolean fits_in_partition(int partition, int process_size){
         return partitions[partition].ItFits(process_size);
     }
-   
-
-//    public static void main(String[] args) {
-//        Mem mem = new Mem();
-//        // check if partitions are ok
-//        if (mem.prim_part + mem.seg_part + mem.ter_part + mem.cuarta_part != 1024){
-//            System.out.println("Error en la asignación de particiones de memoria");
-//            System.out.println("La suma de las particiones es: " + (mem.prim_part + mem.seg_part + mem.ter_part + mem.cuarta_part));
-//        } else {
-//            System.out.println("Particiones de memoria asignadas correctamente");
-//        }
-//    }
 }
--- src/MemorySimulator/Memory/Partition.java
+++ src/MemorySimulator/Memory/Partition.java
@@ -1,39 +1,39 @@
 package MemorySimulator.Memory;
 
 public class Partition {
-    private final short memory_start;
-    private final short memory_end;
-    private final short memory_size;
+    private final int memory_start;
+    private final int memory_end;
+    private final int memory_size;
     private boolean is_free = true;
     private String process_name;
 
 
-    public Partition(short memory_start, short memory_size) {
+    public Partition(int memory_start, int memory_size) {
         this.memory_start = memory_start;
         this.memory_size = memory_size;
-        this.memory_end = (short) (memory_start + memory_size);
+        this.memory_end = memory_start + memory_size;
     }
 
-    public short getMemory_end() {
+    public int getMemory_start() {
+        return memory_start;
+    }
+
+    public int getMemory_end() {
         return memory_end;
     }
 
-    public short getMemory_size() {
+    public int getMemory_size() {
         return memory_size;
-    }
-
-    public short getMemory_start() {
-        return memory_start;
     }
 
     public boolean IsFree() {
         return is_free;
     }
-    public boolean ItFits(short process_size) {
+    public boolean ItFits(int process_size) {
         return process_size <= memory_size;
     }
 
-    public boolean AllocateProcess(String process_name, short process_size, Mem memory) {
+    public boolean AllocateProcess(String process_name, int process_size, Mem memory) {
         if (is_free && ItFits(process_size)) {
             this.process_name = process_name;
             is_free = false;
@@ -46,9 +46,4 @@ public class Partition {
         is_free = true;
         process_name = null;
     }
-
-//    public boolean Overlaps(Partition partition) {
-//        return (this.memory_start >= partition.memory_start && this.memory_start <= partition.memory_end) ||
-//                (this.memory_end >= partition.memory_start && this.memory_end <= partition.memory_end);
-//    }
 }
