8138e48eb38acf26f3aedc1ea29b94394667789e
lemisora <leonardo.michel.dom@gmail.com>
2024-09-02 23:19:12 -0600

    Preparación del código para la reimplementación de la clase scheduler

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -6,27 +6,29 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         // Obtener el número de procesadores disponibles de la computadora
         // puede ser necesario poner un numero menor si se quiere probar con menos procesadores
-        int numProcesadores = (Runtime.getRuntime().availableProcessors());
-        Mem memory = new Mem();
+//        int numProcesadores = (Runtime.getRuntime().availableProcessors());
+//        Mem memory = new Mem();
+//
+//        scheduler scheduler1 = new scheduler(
+//            TaskGenerator.importTasksFromFile(
+//            "tasks.txt"),
+//            "Lista Default",
+//            4,
+//            memory);
+//
+//
+//        Sched2 sched2 = new Sched2(
+//            TaskGenerator.importTasksFromFile(
+//            "tasks.txt"),
+//            "Lista Default",
+//            numProcesadores);
+//
+//        long startTime = System.currentTimeMillis();
+//        scheduler1.run();
+//        sched2.run();
+//        long endTime = System.currentTimeMillis();
+//        long executionTime = endTime - startTime;
+//        System.out.println("Execution time: " + executionTime + " milliseconds");
 
-        scheduler scheduler1 = new scheduler(
-            TaskGenerator.importTasksFromFile(
-            "tasks.txt"),
-            "Lista Default",
-            4,
-            memory);
-
-
-        Sched2 sched2 = new Sched2(
-            TaskGenerator.importTasksFromFile(
-            "tasks.txt"),
-            "Lista Default",
-            numProcesadores);
-
-        long startTime = System.currentTimeMillis();
-        scheduler1.run();
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        System.out.println("Execution time: " + executionTime + " milliseconds");
     }
 }
