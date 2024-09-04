5d2aa021feeaa29b612037998183f70f5a86d10e
lemisora <leonardo.michel.dom@gmail.com>
2024-09-03 22:33:28 -0600

    Depuración de código, la clase está con lo necesario

--- src/MemorySimulator/Memory/Mem.java
+++ src/MemorySimulator/Memory/Mem.java
@@ -3,18 +3,15 @@ package MemorySimulator.Memory;
 public class Mem {
     private final int MEM_RANGE = 1024; // La memoria tiene un rango de 1024 bytes
     private final int MEM_PARTS = 4;   //Cantidad de particiones que tendrá la memoria
-
-    public char[] memory; // tiene la informacion de la memoria
     public Partition [] partitions; //Con un arreglo del objeto partition se mandarán a llamar los métodos para manejar memoria
 
     //Tamanios de las particiones
-    public int prim_part = 230;
-    public int seg_part = 120;
-    public int ter_part = 500;
-    public int cuarta_part = 174;
+    public final int prim_part = 230;
+    public final int seg_part = 120;
+    public final int ter_part = 500;
+    public final int cuarta_part = 174;
     
     public Mem(){
-        memory = new char[MEM_RANGE];
         // Crear particiones
         partitions = new Partition[MEM_PARTS];
         //La primera particion comienza en 0 y termina en 230
@@ -30,26 +27,22 @@ public class Mem {
             System.out.println("Tamaño de la partición "+(i+1)+": "+partitions[i].getMemory_size()+"\nInicio: "+partitions[i].getMemory_start()+"\tFin: "+partitions[i].getMemory_end());
         }
 
-        System.out.println("Se ha inicializado la memoria correctamente");
-    }
-
-    public boolean partition_is_free(int partition){
-        return partitions[partition].IsFree();
+        if(prim_part+seg_part+ter_part+cuarta_part == MEM_RANGE){
+            System.out.println("Se ha inicializado la memoria correctamente");
+        } else {
+            System.err.println("No se ha podido inicializar la memoria correctamente");
+        }
     }
 
     public int MemoryParts(){
         return MEM_PARTS;
     }
 
-    public boolean partition_allocate_process(int partition, String process_name, short process_size){
+    public boolean partition_allocate_process(int partition, String process_name,  int process_size){
         return partitions[partition].AllocateProcess(process_name, process_size, this);
     }
 
     public void partition_deallocate_process(int partition){
         partitions[partition].DeallocateProcess(this);
-    }
-
-    public boolean fits_in_partition(int partition, int process_size){
-        return partitions[partition].ItFits(process_size);
     }
 }
