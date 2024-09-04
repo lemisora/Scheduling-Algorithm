bcb9467146c900e78aa3a989b77d82b27944e1e0
lemisora <leonardo.michel.dom@gmail.com>
2024-09-04 00:39:42 -0600

    Readaptación del main para usar TaskGenerator

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -8,30 +8,37 @@ import MemorySimulator.Tasks.TaskGenerator;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Mem memory = new Mem();
+        Task[] tareas;
+        ListaLigada[] colas;
         // Creamos 4 colas para las 4 particiones
-        ListaLigada[] colas = new ListaLigada[memory.MemoryParts()];
-        for (int i = 0; i < colas.length; i++) {
-            colas[i] = new ListaLigada();
-        }
+//        ListaLigada[] colas = new ListaLigada[memory.MemoryParts()];
+//        for (int i = 0; i < colas.length; i++) {
+//            colas[i] = new ListaLigada();
+//        }
 
-        // Creamos algunas tareas de ejemplo y las distribuimos entre las colas
-        Task tarea1 = new Task(" 1",100, 5);
-        Task tarea2 = new Task(" 2",200, 3);
-        Task tarea3 = new Task(" 3",300, 7);
-        Task tarea4 = new Task(" 4",50, 2);
+        tareas = TaskGenerator.generateManyTasks(20);
+        TaskGenerator.exportTasksToFile(tareas);
+        colas = TaskGenerator.importTasksFromFileToArray("tasks.txt");
 
-        colas[0].agregarNodo(tarea1);  // Asignada a la partición 1
-        colas[1].agregarNodo(tarea2);  // Asignada a la partición 2
-        colas[2].agregarNodo(tarea3);  // Asignada a la partición 3
-        colas[3].agregarNodo(tarea4);  // Asignada a la partición 4
+//        // Creamos algunas tareas de ejemplo y las distribuimos entre las colas
+//        Task tarea1 = new Task(" 1",100, 5);
+//        Task tarea2 = new Task(" 2",200, 3);
+//        Task tarea3 = new Task(" 3",300, 7);
+//        Task tarea4 = new Task(" 4",50, 2);
+//
+//        colas[0].agregarNodo(tarea1);  // Asignada a la partición 1
+//        colas[1].agregarNodo(tarea2);  // Asignada a la partición 2
+//        colas[2].agregarNodo(tarea3);  // Asignada a la partición 3
+//        colas[3].agregarNodo(tarea4);  // Asignada a la partición 4
 
         // Iniciamos el scheduler con las colas y la memoria
         scheduler sched = new scheduler(colas, memory);
         long startTime = System.currentTimeMillis();
         sched.start();
+        sched.join();
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
-        System.out.println("Execution time: " + executionTime + " milliseconds");
+        System.out.println("Tiempo de ejecución: " + (executionTime/1000) + " segundos");
 
     }
 }
