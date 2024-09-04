4206565f602099c7e883643210038332141e1d13
lemisora <leonardo.michel.dom@gmail.com>
2024-09-03 22:36:20 -0600

    Adaptación del main a las nuevas necesidades de la clase scheduler, ejecuta correctamente, falta hacerlo con las tareas leídas desde un archivo

--- src/MemorySimulator/Main.java
+++ src/MemorySimulator/Main.java
@@ -1,34 +1,37 @@
 package MemorySimulator;
 import MemorySimulator.Memory.Mem;
 import MemorySimulator.Scheduler.*;
+import MemorySimulator.Tasks.Task;
+import MemorySimulator.Queue.*;
+
 import MemorySimulator.Tasks.TaskGenerator;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        // Obtener el número de procesadores disponibles de la computadora
-        // puede ser necesario poner un numero menor si se quiere probar con menos procesadores
-//        int numProcesadores = (Runtime.getRuntime().availableProcessors());
-//        Mem memory = new Mem();
-//
-//        scheduler scheduler1 = new scheduler(
-//            TaskGenerator.importTasksFromFile(
-//            "tasks.txt"),
-//            "Lista Default",
-//            4,
-//            memory);
-//
-//
-//        Sched2 sched2 = new Sched2(
-//            TaskGenerator.importTasksFromFile(
-//            "tasks.txt"),
-//            "Lista Default",
-//            numProcesadores);
-//
-//        long startTime = System.currentTimeMillis();
-//        scheduler1.run();
-//        sched2.run();
-//        long endTime = System.currentTimeMillis();
-//        long executionTime = endTime - startTime;
-//        System.out.println("Execution time: " + executionTime + " milliseconds");
+        Mem memory = new Mem();
+        // Creamos 4 colas para las 4 particiones
+        ListaLigada[] colas = new ListaLigada[memory.MemoryParts()];
+        for (int i = 0; i < colas.length; i++) {
+            colas[i] = new ListaLigada();
+        }
+
+        // Creamos algunas tareas de ejemplo y las distribuimos entre las colas
+        Task tarea1 = new Task(" 1",100, 5);
+        Task tarea2 = new Task(" 2",200, 3);
+        Task tarea3 = new Task(" 3",300, 7);
+        Task tarea4 = new Task(" 4",50, 2);
+
+        colas[0].agregarNodo(tarea1);  // Asignada a la partición 1
+        colas[1].agregarNodo(tarea2);  // Asignada a la partición 2
+        colas[2].agregarNodo(tarea3);  // Asignada a la partición 3
+        colas[3].agregarNodo(tarea4);  // Asignada a la partición 4
+
+        // Iniciamos el scheduler con las colas y la memoria
+        scheduler sched = new scheduler(colas, memory);
+        long startTime = System.currentTimeMillis();
+        sched.start();
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        System.out.println("Execution time: " + executionTime + " milliseconds");
 
     }
 }
