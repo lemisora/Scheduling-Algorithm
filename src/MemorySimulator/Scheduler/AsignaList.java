428288ad1e9fdf9127718ab15d617480d557122b
anteba <jose.tejedabar@alumno.buap.mx>
2024-09-04 15:13:56 -0600

    Se crean contadores que ayudan a medir el tamaño en tiempo de una lista en especifico y se modifica la asignacion de nodos por lista.

--- src/MemorySimulator/Scheduler/AsignaList.java
+++ src/MemorySimulator/Scheduler/AsignaList.java
@@ -5,7 +5,9 @@ import MemorySimulator.Tasks.Task;
 import MemorySimulator.Tasks.TaskGenerator;
 
 public class AsignaList {
+    private int aux1 =0, aux2 =0, aux3=0, aux4=0; 
     private ListaLigada lis;
+    
     ListaLigada lis1=new  ListaLigada();
     ListaLigada lis2=new  ListaLigada();
     ListaLigada lis3=new  ListaLigada();
@@ -24,14 +26,33 @@ public class AsignaList {
             Task task =referencia.getTask();
             //230, 120, 500, 174
             if(task.getTamano()<121){
-                lista1(task);
-                lis.quitarNodInic();
+                if(aux1<aux2){
+                    lista1(task);
+                    lis.quitarNodInic();
+                }
+                
+
+
             }
-            if(task.getTamano()>120 && task.getTamano()<175 ){
-                lista2(task);
-                lis.quitarNodInic();
+            //lo que se podria hacer es modificar los menor que para que asi se ordene con cada if 
+            //cada if deberia de tener un if en el que dependiendo de auxN se asigne o no 
+            if( task.getTamano()<175 ){
+                if(aux2<aux1){
+                    lista1(task);
+                    lis.quitarNodInic();
+                }
+                if(aux2<aux3 && task.getTamano()>120 ){
+                    lista2(task);
+                    lis.quitarNodInic();
+                }
+                
             }
-            if(task.getTamano()>174 && task.getTamano()<231 ){
+            if(task.getTamano()>120 && task.getTamano()<231 ){
+                if(aux3<aux2){
+                    lista2(task);
+                    lis.quitarNodInic();
+                }
+                //aun no terminado, pausado
                 lista3(task);
                 lis.quitarNodInic();
             }
@@ -43,28 +64,63 @@ public class AsignaList {
                 System.out.println("Proceso: "+ task.getName()+ " eliminado");
                 lis.quitarNodInic();
             }
+            if(getAux2()<getAux1()){
+                //lis1. = lis2.agregarNodo();
+             lis2.agregarNodo(task);
+
+
+            }
             referencia = referencia.getSig();
         
         }
     }
 
+    public int getAux1(){
+        return aux1;
+    }
+
+    public int getAux2(){
+        return aux2;
+    }
+
+    public int getAux3(){
+        return aux3;
+    }
+
+    public int getAux4(){
+        return aux4;
+    }
+
+    //Muy posible de BORRAR
+    public void reasig(){
+        if(getAux1()<getAux2()){
+            lis1.agregarNodo(null);
+        }
+    }
+
     public ListaLigada lista1(Task proces ){
         lis1.agregarNodo(proces);
+        aux1 = aux1+proces.getTiempo();
         return lis1;
     }
 
+    
+
     public ListaLigada lista2(Task proces ){
         lis2.agregarNodo(proces);
+        aux2 = aux2+proces.getTiempo();
         return lis2;
     }
 
     public ListaLigada lista3(Task proces ){
         lis3.agregarNodo(proces);
+        aux3 = aux3+proces.getTiempo();
         return lis3;
     }
 
     public ListaLigada lista4(Task proces ){
         lis4.agregarNodo(proces);
+        aux4 = aux4+proces.getTiempo();
         return lis4;
     }
 
