081a299862c34111e86f4758caf266f7c3897d85
anteba <jose.tejedabar@alumno.buap.mx>
2024-09-05 00:11:38 -0600

    Se mandan mensajes para hacer pruebas.

--- src/MemorySimulator/Scheduler/Sched2.java
+++ src/MemorySimulator/Scheduler/Sched2.java
@@ -16,12 +16,13 @@ public class Sched2 implements Runnable {
     @Override
     public void run(){
         Nodo referencia = lis.getHead();
-        System.out.println("Nombre de la lista: "+nombreLista );
+        System.out.println("Nombre de la listaaa: "+nombreLista );
         while(referencia != null){
+            
             Task task =referencia.getTask();
-            System.out.println("Proceso actual: "+task.getName()+" Tamano: "+task.getTamano()+" Tiempo: "+task.getTiempo() + "\n ");
+            System.out.println("Proceso actual: "+task.getName()+" Tamano: "+task.getTamano()+" Tiempo: "+task.getTiempo() + "Nombre de la lista: "+nombreLista + "\n ");
             try{
-                Thread.sleep(task.getTiempo()*1000);
+                Thread.sleep(task.getTiempo()*100);
             }catch(InterruptedException e){
                 System.out.println(nombreLista + "fue interrumpida");
             }
