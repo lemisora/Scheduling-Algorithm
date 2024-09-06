baa6734e1ba9ef3d5029009ea173c139989affd3
Rain <114127654+RainBWP@users.noreply.github.com>
2024-09-06 19:24:05 +0000

    Version del programa en C++

---
+++ src/MemorySimulator/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class nodo
+{
+private:
+    /* data */
+public:
+    nodo(int dato);
+    ~nodo();
+};
+
+nodo::nodo(int dato)
+{
+}
+
+nodo::~nodo()
+{
+}
+
+
+
+
+int main() {
+    // Your code here
+
+
+    return 0;
+}
